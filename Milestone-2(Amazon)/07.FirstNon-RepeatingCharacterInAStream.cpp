982a5994cd8db81a16f12415bf50bded7174b15f
AasthaJaipuria <70774035+AasthaJaipuria@users.noreply.github.com>
2022-01-09 20:04:09 +0530

    Create 07.FirstNon-RepeatingCharacterInAStream.cpp

---
+++ Milestone-2(Amazon)/07.FirstNon-RepeatingCharacterInAStream.cpp
@@ -0,0 +1,26 @@
+//GFG Question: https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    string ans = "";
+		    int hash[26];
+		    for(int i=0; i<26; i++)
+		    hash[i]=0;
+		    int j=0;
+		    for(int i=0; i<A.length(); i++)
+		    {
+		       hash[int(A[i]-'a')]++;
+		       while(j<=i && hash[int(A[j]-'a')]>1)
+		       {
+		           j++;
+		       }
+		       if(j<=i)
+		       ans+=A[j];
+		       else ans+="#";
+		    }
+		    return ans;
+		}
+
+};
